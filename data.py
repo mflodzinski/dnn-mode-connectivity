1359632ef4fe4b5917884e1d138f5fe780ccb3ee
Maciej Lodzinski <mf.lodzinski@gmail.com>
2026-01-24 21:00:11 +0100

    test trainsform for fmnist on original splits

--- data.py
+++ data.py
@@ -77,50 +77,11 @@ def loaders(dataset, path, batch_size, num_workers, transform_name, use_test=Fal
     path = os.path.join(path, dataset.lower())
     transform = getattr(getattr(Transforms, dataset), transform_name)
     train_set = ds(path, train=True, download=True, transform=transform.test)
+
+    print('You are going to run models on the test set. Are you sure?')
     test_set = ds(path, train=False, download=True, transform=transform.test)
-
-    # If use_test is True, shuffle the order within each split (train stays train, test stays test)
-    if use_test:
-        print(f'Shuffling {dataset}: train ({len(train_set.data)}) and test ({len(test_set.data)}) splits independently')
-
-        # Store original data type (tensor for FashionMNIST, numpy for CIFAR)
-        train_is_tensor = isinstance(train_set.data, torch.Tensor)
-        test_is_tensor = isinstance(test_set.data, torch.Tensor)
-
-        # Shuffle train set
-        train_size = len(train_set.data)
-        train_indices = np.arange(train_size)
-        np.random.seed(412)  # Fixed seed for reproducibility
-        np.random.shuffle(train_indices)
-
-        if train_is_tensor:
-            train_set.data = train_set.data[train_indices]
-            train_set.targets = torch.tensor(train_set.targets)[train_indices].tolist()
-        else:
-            train_set.data = train_set.data[train_indices]
-            if isinstance(train_set.targets, list):
-                train_set.targets = [train_set.targets[i] for i in train_indices]
-            else:
-                train_set.targets = np.array(train_set.targets)[train_indices].tolist()
-
-        # Shuffle test set
-        test_size = len(test_set.data)
-        test_indices = np.arange(test_size)
-        np.random.seed(413)  # Different seed for test set
-        np.random.shuffle(test_indices)
-
-        if test_is_tensor:
-            test_set.data = test_set.data[test_indices]
-            test_set.targets = torch.tensor(test_set.targets)[test_indices].tolist()
-        else:
-            test_set.data = test_set.data[test_indices]
-            if isinstance(test_set.targets, list):
-                test_set.targets = [test_set.targets[i] for i in test_indices]
-            else:
-                test_set.targets = np.array(test_set.targets)[test_indices].tolist()
-
     val_set = None
-
+    
 
     # pin_memory not supported on MPS
     pin_mem = not (hasattr(torch.backends, 'mps') and torch.backends.mps.is_available())
